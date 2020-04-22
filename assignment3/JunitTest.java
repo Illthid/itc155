829bfd3e7d6ab5fdc31f183fe591d3bcc5327f63
David Barnette <55769751+Illthid@users.noreply.github.com>
2020-04-21 19:07:54 -0700

    Update JunitTest.java

--- assignment3/JunitTest.java
+++ assignment3/JunitTest.java
@@ -2,7 +2,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class RemovEvLenTest {
+class RemoveEvenLengthTest {
 
 	@Test
 	public void testDefEven() {
