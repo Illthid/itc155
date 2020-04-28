5be08f5abce82bd8917bbca2421d64c3fa99e39a
David Barnette <55769751+Illthid@users.noreply.github.com>
2020-04-28 13:40:52 -0700

    Update RemoveEvenLengthTest.java

--- assignment3/RemoveEvenLengthTest.java
+++ assignment3/RemoveEvenLengthTest.java
@@ -1,16 +1,27 @@
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import org.junit.Test;
 
-import org.junit.jupiter.api.Test;
+class removeEvenLengthTest extends removeEvenLength{
+    @Test
+    public void testRemoveEvenLength() {
+        ArrayList<String> testlist = new ArrayList<String>();
+        testlist.add("walk");
+        testlist.add("talk");
+        testlist.add("speak");
+        testlist.add("run");
+        testlist.add("falling");
+        testlist.add("scream");
 
-class RemoveEvenLengthTest {
+        ArrayList<String> testlist2 = new ArrayList<String>();
+        testlist2.add("walking");
+        testlist2.add("talking");
+        testlist2.add("speaking");
+        
+        removeEvenLength(testlist);
 
-	@Test
-	public void testDefEven() {
-		ArrayList<String> testlist = new ArrayList<String>();
-		testlist.add("even");
-		(element.length == % 2 = 0));
-		assertEquals(true); 
-		fail("function not correct");
-	}
+        assertEquals(testlist,testlist2);
 
-}
+
+        }
+    }
