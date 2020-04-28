0c5fecb1ba9c9f71059d39461be394e23da90fa5
David Barnette <55769751+Illthid@users.noreply.github.com>
2020-04-28 13:44:02 -0700

    Update RemoveEvenLengthTest.java

--- assignment3/RemoveEvenLengthTest.java
+++ assignment3/RemoveEvenLengthTest.java
@@ -12,14 +12,14 @@ class removeEvenLengthTest extends removeEvenLength{
         testlist.add("run");
         testlist.add("falling");
         testlist.add("scream");
+        assertEquals(testlist.size(), 6);
 
         ArrayList<String> testlist2 = new ArrayList<String>();
         testlist2.add("walking");
         testlist2.add("talking");
         testlist2.add("speaking");
-        
-        removeEvenLength(testlist);
 
+        removeEvenLength(testlist);
         assertEquals(testlist,testlist2);
 
 
