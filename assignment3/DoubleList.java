49092abee8072a692532ed79069daa801388424c
David Barnette <55769751+Illthid@users.noreply.github.com>
2020-04-28 13:41:50 -0700

    Update DoubleList.java

--- assignment3/DoubleList.java
+++ assignment3/DoubleList.java
@@ -1,36 +1,37 @@
-//*******************************************************************
+///*******************************************************************
 // DoubleList.java
 //
 // A class that takes an ArrayList<String> and replaces every string
-// (element) with two of that same string. Includes a jUnit test.
-// 
+// (element) with two of that same string.
+//
 // David Barnette
-// 4/262020
+// 4/26/2020
 //*******************************************************************
 
 import java.util.ArrayList;
 
-public class DoubleList {
+public class doubleList {
 
-	public static void main(String[] args) {
-		ArrayList<String> list = new ArrayList<String>();
-    
-		list.add("how");
-		list.add("are");
-		list.add("you?");
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
 
-		System.out.println("Before doubling elements:\n" + list);
-		System.out.println();
-		doubleList(list);
-		System.out.println("After doubling elements:\n" + list);
-	}
+        list.add("how");
+        list.add("are");
+        list.add("you?");
 
-	public static void doubleList(ArrayList<String> name) {
-		int arraySize = name.size();
-		
-		for (int i = 0; i < arraySize; i++) {
-			name.add(i * 2 + 1, name.get(i * 2));
-		}
+        System.out.println("Before doubling elements:\n" + list);
+        System.out.println();
+        doubleList(list);
+        System.out.println("After doubling elements:\n" + list);
+    }
 
-	}
-	
+    public static void doubleList(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            String copy = list.get(i);
+            list.add(i+1, copy);
+            i++;
+        }
+        }
+
+    }
+
