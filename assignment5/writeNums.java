ce4b5730f74e68f87b37e4f4b5921957541fcba2
David Barnette <55769751+Illthid@users.noreply.github.com>
2020-05-05 10:43:50 -0700

    Create writeNums.java

---
+++ assignment5/writeNums.java
@@ -0,0 +1,26 @@
+/*
+* Write a method writeNums that accepts an integer parameter n and prints 
+* the first n integers starting with 1 in sequential order, separated by
+* commas. Your method should throw an IllegalArgumentException if passed 
+* a value less than 1. 
+*/
+public static void main(String[] args) {
+		writeNums(1);
+		System.out.println();
+		writeNums(5);
+		System.out.println();
+		writeNums(12);
+    System.out.println();
+    
+
+	}
+public static void writeNums(int n) {
+    if (n < 1) {
+        throw new IllegalArgumentException();
+    } else if (n == 1) {
+        System.out.print("1");
+    } else {
+        writeNums(n - 1);
+        System.out.print(", " + n);
+    }
+}
