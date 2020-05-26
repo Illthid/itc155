f1bf1af233f63ad541deb592ebb09d262f078dab
David Barnette <55769751+Illthid@users.noreply.github.com>
2020-05-26 11:19:55 -0700

    Update PhotoLineups.java

--- midTerm/PhotoLineups.java
+++ midTerm/PhotoLineups.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 import java.util.ArrayList;
-//Given a HashMap pre-filled with student names as keys and grades as values, 
-//complete main() by reading in the name of a student, outputting their original
-//grade, and then reading in and outputting their new grade.
+
+//Problem 1
+//Write a program that lists all ways people can line up for a photo 
+//(all permutations of a list of Strings). The program will read a list of
+//one word names (until -1), and use a recursive method to create and output 
+//all possible orderings of those names, one ordering per line.
 //05/26/2020
 //David Barnette
 
