08ce0f18e297081c39b5a2f1b4a067eb12d72a12
David Barnette <55769751+Illthid@users.noreply.github.com>
2020-06-17 19:37:44 -0700

    Update hasTwoConsecutive.java

--- finalassignment/hasTwoConsecutive.java
+++ finalassignment/hasTwoConsecutive.java
@@ -3,24 +3,24 @@
  * BJP5 Exercise 16.6: Method hasTwoConsecutive 
  * FIRST METHOD defined after constructor in LinkedIntList.java
  */
-public class ConsecutiveClient {
+public class Consecutive_Client {
 
 	public static void main(String[] args) {
 
 		LinkedIntList list1 = new LinkedIntList();
-		list1.add(1);
-		list1.add(2);
 		list1.add(3);
-		list1.add(4);
+		list1.add(6);
+		list1.add(9);
+		list1.add(12);
 
 		System.out.println("");
 		System.out.println("Original list sequence" + list1);
 		System.out.println("Method hasTwoConsecutive Test: >>> " + list1.hasTwoConsecutive());
 		System.out.println();
 
-		list1.add(12);
-		list1.add(2);
-		list1.set(1, 3);
+		list1.add(15);
+		list1.add(5);
+		list1.set(3, 7);
 
 		System.out.println("The List has been modified >>> " + list1);
 		System.out.println("Method hasTwoConsecutive Test: >>> " + list1.hasTwoConsecutive());
