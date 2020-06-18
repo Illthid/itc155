5740c1f7d00259d0398cd8812f24a7770b429929
David Barnette <55769751+Illthid@users.noreply.github.com>
2020-06-17 20:26:01 -0700

    Update hasTwoConsecutive.java

--- Final/hasTwoConsecutive.java
+++ Final/hasTwoConsecutive.java
@@ -15,24 +15,91 @@ import java.util.*;
 //any assumptions about how many elements are in the list.
 //Assume that you are adding this method to the LinkedIntList class as defined below:
 
-public class LinkedIntList {
 
-	private ListNode front;
-
-	public LinkedIntList() {
-		front = null;
-	}
-
-	public boolean hasTwoConsecutive() {
-		if (front == null)
-			return false;
-
-		ListNode current = front;
-		while (current.next != null) {
-			if (current.data + 1 == current.next.data)
-				return true;
-			current = current.next;
-		}
-		return false;
-	}
 }
+class listNode{
+    int data;
+    listNode next;
+}
+
+public class hasTwoConsecutive {
+    listNode first;
+    public boolean hasTwoConsecutive() {
+        if(first == null) {
+            return false;
+        }
+        listNode prev = first;
+        listNode current = prev.next;
+
+        while(current != null) {
+            if(prev.data + 1 == current.data) {
+                return true;
+            }
+            prev = current;
+            current = prev.next;
+        }
+        return false;
+    }
+    public void insertInt(int data) {
+        listNode node=new listNode();
+        node.data=data;
+        node.next=null;
+
+        if(first==null) {
+            first=node;
+        }
+        else {
+            listNode n=first;
+            while(n.next!=null) {
+                n=n.next;
+            }
+            n.next=node;
+        }
+    }
+    public void insertStart(int data){
+        listNode node=new listNode();
+        node.data=data;
+        node.next=null;
+        node.next=first;
+        first = node;
+    }
+    public void insertSpecific(int index, int data) {
+        listNode node=new listNode();
+        node.data=data;
+        node.next=null;
+        if(index==0) {
+            insertStart(data);
+        }
+        else {
+            listNode n = first;
+            for(int i=0;i<index;i++) {
+                n=n.next;
+            }
+            node.next=n.next;
+            n.next=node;
+        }
+    }
+    public void deleteSpecific(int index) {
+        if(index==0) {
+            first=first.next;
+        }
+        else {
+            listNode n = first;
+            listNode nn = null;
+            for (int i=0;i<index;i++) {
+                n=n.next;
+            }
+            nn=n.next;
+            n.next=nn.next;
+        }
+    }
+    public void show() {
+        listNode node = first;
+        while (node.next!=null) {
+            System.out.println(node.data);
+            node=node.next;
+        }
+        System.out.println(node.data);
+    }
+}
+
