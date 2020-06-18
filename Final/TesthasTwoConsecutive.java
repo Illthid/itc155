d744d469d83a435054a1af96197a103796ff8ac5
David Barnette <55769751+Illthid@users.noreply.github.com>
2020-06-17 20:15:37 -0700

    Update TesthasTwoConsecutive.java

--- Final/TesthasTwoConsecutive.java
+++ Final/TesthasTwoConsecutive.java
@@ -7,11 +7,11 @@ class LinkedIntListJunitTest {
 	void testFullTrue() {
 
 		LinkedIntList list = new LinkedIntList();
-		list.add(1);
 		list.add(2);
-		list.add(3);
 		list.add(4);
-		list.add(5);
+		list.add(6);
+		list.add(8);
+		list.add(10);
 
 		assertTrue(list.hasTwoConsecutive());
 	}
