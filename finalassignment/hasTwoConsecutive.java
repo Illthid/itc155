bb9e5b3a62d93ada4c8bf54a3076f52b79724048
David Barnette <55769751+Illthid@users.noreply.github.com>
2020-06-17 19:36:29 -0700

    Update hasTwoConsecutive.java

--- finalassignment/hasTwoConsecutive.java
+++ finalassignment/hasTwoConsecutive.java
@@ -1,23 +1,29 @@
-/*
-* Write a method hasTwoConsecutive that returns whether or not a list of
-* integers has two adjacent numbers that are consecutive integers (true 
-* if such a pair exists and false otherwise). 
-*/
+/***************************************************************************
+ * ITC155 - Final - Problem 2
+ * BJP5 Exercise 16.6: Method hasTwoConsecutive 
+ * FIRST METHOD defined after constructor in LinkedIntList.java
+ */
+public class ConsecutiveClient {
 
-public boolean hasTwoConsecutive() {
-    ListNode current = front;
-    
-    if (front == null) {
-        return false;
-    } else {
-    
-        while (current.next != null) {
-            if (current.data + 1 == current.next.data) {
-                return true;
-            }
-            current = current.next;
-        }
-        
-        return false;
-    }
+	public static void main(String[] args) {
+
+		LinkedIntList list1 = new LinkedIntList();
+		list1.add(1);
+		list1.add(2);
+		list1.add(3);
+		list1.add(4);
+
+		System.out.println("");
+		System.out.println("Original list sequence" + list1);
+		System.out.println("Method hasTwoConsecutive Test: >>> " + list1.hasTwoConsecutive());
+		System.out.println();
+
+		list1.add(12);
+		list1.add(2);
+		list1.set(1, 3);
+
+		System.out.println("The List has been modified >>> " + list1);
+		System.out.println("Method hasTwoConsecutive Test: >>> " + list1.hasTwoConsecutive());
+	}
+
 }
